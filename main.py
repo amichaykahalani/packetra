81afb77ccad33e536a5ad23ee5b46d12c1d3bbe6
amichay kahalani <amichay.kahalani@gmail.com>
2025-10-21 23:14:14 +0300

    beta1.1 update

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from DNS import DNS
 from NetworkTransfer import NetworkTransfer
 
 def main():
-    question_object = DNS("x.com", record_type="IPv4")
+    question_object = DNS("youtube.com", record_type="IPv4")
     question = question_object.to_binary()
-    answer_object = DNS("x.com", is_response=True, record_type="IPv6")
+    answer_object = DNS("youtube.com", is_response=True, record_type="IPv6")
     answer_object.deserializer(NetworkTransfer.send_and_received(question,
                                                                 '8.8.8.8',
                                                                 53,
